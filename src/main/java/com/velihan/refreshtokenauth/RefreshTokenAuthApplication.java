ed5da8b2de18f3d88898486f09a06defb33aecd7
Velihan Gözek <velihangozek@hotmail.com>
2026-04-25 21:38:48 +0300

    chore: initialize Spring Boot refresh token auth project

---
+++ src/main/java/com/velihan/refreshtokenauth/RefreshTokenAuthApplication.java
@@ -0,0 +1,13 @@
+package com.velihan.refreshtokenauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RefreshTokenAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RefreshTokenAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/velihan/refreshtokenauth/RefreshTokenAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.velihan.refreshtokenauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RefreshTokenAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
