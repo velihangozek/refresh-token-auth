e16d62f65ea2348589be6e9bf398f763fd84e3f2
Velihan Gözek <velihangozek@hotmail.com>
2026-04-25 22:00:03 +0300

    feat(repository): add user and refresh token repositories

---
+++ src/main/java/com/velihan/refreshtokenauth/repository/RefreshTokenRepository.java
@@ -0,0 +1,14 @@
+package com.velihan.refreshtokenauth.repository;
+
+import com.velihan.refreshtokenauth.entity.RefreshToken;
+import com.velihan.refreshtokenauth.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, UUID> {
+    Optional<RefreshToken> findByToken(String token);
+    List<RefreshToken> findAllByUser(User user);
+}
---
+++ src/main/java/com/velihan/refreshtokenauth/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.velihan.refreshtokenauth.repository;
+
+import com.velihan.refreshtokenauth.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+    Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
+}
