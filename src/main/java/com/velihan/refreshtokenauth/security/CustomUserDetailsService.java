1c466f5081cc372e474f6fa885b1096e3a66e3a5
Velihan Gözek <velihangozek@hotmail.com>
2026-04-25 22:23:50 +0300

    feat(security): add custom user details service

---
+++ src/main/java/com/velihan/refreshtokenauth/security/CustomUserDetailsService.java
@@ -0,0 +1,28 @@
+package com.velihan.refreshtokenauth.security;
+
+import com.velihan.refreshtokenauth.entity.User;
+import com.velihan.refreshtokenauth.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + email));
+
+        return org.springframework.security.core.userdetails.User.builder()
+                .username(user.getEmail())
+                .password(user.getPassword())
+                .roles(user.getRole())
+                .build();
+    }
+}
